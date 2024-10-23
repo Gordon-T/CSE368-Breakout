8a7baa87b8648e4d7eb86be1ac847e06924348bd
Gordon-T <93834879+Gordon-T@users.noreply.github.com>
2024-10-22 20:41:02 -0400

    Added a random agent

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import gymnasium as gym
 import ale_py
+import time
 
 gym.register_envs(ale_py)
-
-env = gym.make('ALE/Breakout-v5')
+env = gym.make('ALE/Breakout-v5', render_mode='human')
 obs, info = env.reset()
 obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+env.render()
+
+for i in range(1000):
+    action = env.action_space.sample()
+
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        obs, info = env.reset()
+
+    time.sleep(0.01)
+
 env.close()
