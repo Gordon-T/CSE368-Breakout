1a58126234b008b84b71860292152b71cab64291
Gordon-T <93834879+Gordon-T@users.noreply.github.com>
2024-11-03 15:43:30 -0500

    Ram observation space

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import gymnasium as gym
 import ale_py
+import time
 import numpy as np
 
 gym.register_envs(ale_py)
-env = gym.make('ALE/Breakout-v5', render_mode='human')
+
+# Using "ram" obs space for now until we figure out how to make use of "rgb"
+env = gym.make('ALE/Breakout-v5', render_mode='human', obs_type="ram")#, frameskip=1)
 obs, info = env.reset()
 env.render()
 
-# TODO: Takes the observation space and tries to find where the ball and paddle x-coordinates
+# TODO: Takes the rgb or grayscale observation space and tries to find where the ball and paddle x-coordinates
 def findCoordinates(obs):
     # Matrix shape of obs should be (210, 160, 3)
 
@@ -21,7 +24,7 @@ def findCoordinates(obs):
 
     return ball_x, paddle_x
 
-# Checks if we should move the paddle to meet the ball
+# Checks if we should move the paddle to meet the ball depending on the coordinates of each
 def getAction(ball_x, paddle_x):
     if ball_x > paddle_x:
         return 2 # Move paddle right
@@ -30,26 +33,29 @@ def getAction(ball_x, paddle_x):
     else:
         return 0 # Do nothing
 
-# Fire the ball
-env.step(1)
+# Fires the first ball
+obs, reward, terminated, truncated, info = env.step(1)
 
 terminated = False
-screen_width = obs.shape[1]
-
 # Agent loop
 while not terminated:
     # Find ball and paddle positions
-    ball_x, paddle_x = findCoordinates(obs)
+    #ball_x, paddle_x = findCoordinates(obs)
+
+    #Fire ball
+    env.step(1)
+    
+    ball_x = obs[99]
+    paddle_x = obs[72]
     
     # Get the appropriate action
     action = getAction(ball_x, paddle_x)
-    
-    # Perfom action
+
+    # Perform action
+    #action = env.action_space.sample()
     obs, reward, terminated, truncated, info = env.step(action)
     
     if terminated or truncated:
         obs, info = env.reset()
-        # Fire the ball again if we lose a life
-        env.step(1)
 
 env.close()
